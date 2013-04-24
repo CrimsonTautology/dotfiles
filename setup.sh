857306bce12f2f584394d4df6638bd49e0ea9a6a
Bill <wsaltzgiver@saa.com>
2013-04-24 10:54:32 -0400

    Added a setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+ln -s ~/bin/dotfiles/_bashrc ~/.bashrc
+ln -s ~/bin/dotfiles/_dircolors ~/.dircolors
+ln -s ~/bin/dotfiles/_gitconfig ~/.gitconfig
+ln -s ~/bin/dotfiles/_inputrc ~/.inputrc
+ln -s ~/bin/dotfiles/_rspec ~/.rspec
+ln -s ~/bin/dotfiles/_tmux.conf ~/.tmux
+ln -s ~/bin/dotfiles/_vimperatorrc ~/.vimperatorrc
