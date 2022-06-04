f4a96b9660ba23092d137d430df2b23af478ae33
Bill <billehs+github@gmail.com>
2022-06-04 17:03:09 -0400

    re-org setup files

--- setup_windows.bat
+++ setup_windows.bat
@@ -1,3 +1,4 @@
+REM setup vim
 (echo source ~/bin/dotfiles/vim/vimrc) > %USERPROFILE%\_vimrc
 (echo source ~/bin/dotfiles/vim/gvimrc) > %USERPROFILE%\_gvimrc
 (echo source ~/bin/dotfiles/vim/vsvimrc) > %USERPROFILE%\_vsvimrc
